7cc251b033bdd0635ea96f53bd705ad216bba6d8
wron <whisnos@163.com>
2019-07-03 14:54:59 +0800

    add

--- xadmin_test/settings.py
+++ xadmin_test/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'xadmin',
+    'xadmin.apps.XAdminConfig',
     'crispy_forms',
     'rest_framework',
 ]
